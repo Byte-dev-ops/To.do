37c2725b34e1025ee7bf9e538423b80cdd7c3cc4
Mukul <mukul.mutreja348@gmail.com>
2026-01-08 19:04:03 +0530

    To Do Web Application
    Added many features and will update it soon

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.getElementById('input-text');
+const addBtn = document.getElementById('add-button');
+const todoList = document.getElementById('todo-list');
+
+function createTodo() {
+    const text = input.value.trim();
+
+    if (text === "") {
+        alert("Please enter any text");
+        return;
+    }
+    const li = document.createElement('li');
+
+    li.innerHTML = `
+    <span class="task-text">${text}</span>
+    <button class="Delete-btn" >Delete</button>
+    `;
+
+    li.querySelector('.task-text').addEventListener('click', function () {
+        this.classList.toggle('completed');
+    });
+
+    li.querySelector('.Delete-btn').addEventListener('click', function () {
+        li.remove();
+    });
+    todoList.appendChild(li);
+    input.value = "";
+}
+addBtn.addEventListener('click', createTodo) ;
+
+input.addEventListener('keypress',(e) => {
+    if(e.key==='Enter') {
+        createTodo();
+    }
+})
